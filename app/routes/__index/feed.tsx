9f0264ecac2ab3b44c9ea7708adb4d69ac89d384
Itamar Sharify <itamarsharify@gmail.com>
2022-06-17 17:29:52 +0300

    chg: fix ui and add episodes dates

--- app/routes/__index/feed.tsx
+++ app/routes/__index/feed.tsx
@@ -1,5 +1,6 @@
-import { Link, useLoaderData, useSearchParams } from "@remix-run/react";
+import { useFetcher, useLoaderData, useSearchParams } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/server-runtime";
+import { useEffect, useState } from "react";
 import EpisodeCount from "~/components/EpisodeCount";
 import type { EpisodeType } from "~/components/Episodes";
 import Episodes from "~/components/Episodes";
@@ -21,18 +22,30 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Feed() {
-  const latestEpisodes = useLoaderData();
-  const [searchParams] = useSearchParams();
-  const limit = searchParams.get("limit") || "5";
-  const { count, collection } = latestEpisodes;
+  let { count, collection } = useLoaderData();
 
+  const fetcher = useFetcher();
+  const [fetch, setFetch] = useState(5);
+
+  useEffect(() => {
+    fetcher.load(`/feed?limit=${fetch}`);
+  }, [fetch, collection]);
+  if (fetcher.data) {
+    count = fetcher.data.count;
+    collection = fetcher.data.collection;
+  }
   return (
     <div className="feed">
       <h1>פיד</h1>
       <EpisodeCount count={count} />
       <Episodes episodes={collection as EpisodeType[]} />
       <div className="more">
-        <Link to={`/feed?limit=${parseInt(limit) + 5}`}>עוד, עוד!</Link>
+        <button
+          onClick={() => setFetch(fetch + 5)}
+          disabled={fetcher.state == "loading"}
+        >
+          עוד, עוד!
+        </button>
       </div>
     </div>
   );
